0fa38f20d19c4344602f892f9aa4770a42cb8846
electrxety <er191712@gmail.com>
2024-04-16 18:20:15 -0400

    solved question 1

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,5 +1,19 @@
-const myForEach = () => {
+//Lesson 1-6 Assignment: HOFs & Callbacks by Eileen
+//challenge is to build from scratch: can't use the real array.forEach, array.map, array.find, array.filter methods
+
+//Question 1: a function that takes two arguments, an array and a callback function. must pass each value from the given array into the provided callback (try a loop). the callback needs to be invoked by the function; only pass the value to the callback (no need to pass anything else). just like the real forEach, myForEach should return undefined and should not alter the passed in array by default. your callback's return value should be ignored.
+const myForEach = (arr, callback) => {
+  for (let el of arr) { //iterating through each element in a given array
+    callback(el); //invoking a given callback function without returning it, passing the value of each element in a given array into it
+  };
 };
+
+//printing q1 test to the console below
+const myNames = ['Alice', 'Bob', 'Charlie', 'Debbie'];
+const result = myForEach(myNames, (name) => console.log(`Hi, ${name}!`)); //Hi, Alice!      Hi, Bob!      Hi, Charlie!     Hi, Debbie!
+
+console.log(result); // undefined
+console.log(myNames); // ['Alice', 'Bob', 'Charlie', 'Debbie']
 
 const myMap = () => {
 };
