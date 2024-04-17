c3c4c9e22f52a2671c5c59d112fcc84c04a6f647
electrxety <er191712@gmail.com>
2024-04-17 01:19:47 -0400

    solved question 5

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,7 +1,7 @@
 //Lesson 1-6 Assignment: HOFs & Callbacks by Eileen
 //challenge is to build from scratch: can't use the real array.forEach, array.map, array.find, array.filter methods
 
-//Question 1: a function that takes two arguments, an array and a callback function. must pass each value from the given array into the provided callback (try a loop). the callback needs to be invoked by the function; only pass the value to the callback (no need to pass anything else). just like the real forEach, myForEach should return undefined and should not alter the passed in array by default. your callback's return value should be ignored.
+//Question 1: a .forEach version that takes two arguments, an array and a callback function. must pass each value from the given array into the provided callback (try a loop). the callback needs to be invoked by the function; only pass the value to the callback (no need to pass anything else). just like the real forEach, myForEach should return undefined and should not alter the passed in array by default. your callback's return value should be ignored.
 const myForEach = (arr, callback) => {
   for (let el of arr) { //iterating through each element in a given array
     callback(el); //invoking a given callback function without returning it, passing the value of each element in a given array into it
@@ -12,10 +12,10 @@ const myForEach = (arr, callback) => {
 // const myNames = ['Alice', 'Bob', 'Charlie', 'Debbie'];
 // const result = myForEach(myNames, (name) => console.log(`Hi, ${name}!`)); //Hi, Alice!      Hi, Bob!      Hi, Charlie!     Hi, Debbie!
 
-// console.log(result); // undefined
-// console.log(myNames); // ['Alice', 'Bob', 'Charlie', 'Debbie']
+// console.log(result); //undefined
+// console.log(myNames); //['Alice', 'Bob', 'Charlie', 'Debbie']
 
-//Question 2: this .map version should return a new array made of the return values of the callback on each item. the callback here does need to return something, because each return value gets fed into the new array. do not modify the original array!
+//Question 2: a .map version should return a new array made of the return values of the callback on each item. the callback here does need to return something, because each return value gets fed into the new array. do not modify the original array!
 const myMap = (arr, callback) => {
   let newArr = []; //initializing the new array to hold the return value of the invocation of a given callback function on a given array, this way original function is not mutated
   for (let el of arr) { //iterating through each element in a given array
@@ -31,7 +31,7 @@ const myMap = (arr, callback) => {
 // console.log(myDoubledNums); //[2, 8, 18, 32]
 // console.log(myNums); //[1, 4, 9, 16] (orig array is unaffected!)
 
-//Question 3: this .find version takes an array and a callback. now, this callback is different than the other two that it must return a boolean (or truthy/falsy value). you'll need to iterate through the given array, and call the callback on each value of the array. if the callback returns true, return the value itself from myFind. if the callback never returns true, then myFind returns undefined. do not modify the original array! this is important: the callback returns true or `false``, the HOF itself returns a value or undefined, ok?
+//Question 3: a .find function version takes an array and a callback. now, this callback is different than the other two that it must return a boolean (or truthy/falsy value). you'll need to iterate through the given array, and call the callback on each value of the array. if the callback returns true, return the value itself from myFind. if the callback never returns true, then myFind returns undefined. do not modify the original array! this is important: the callback returns true or `false``, the HOF itself returns a value or undefined, ok?
 const myFind = (arr, callback) => {
   for (let el of arr) { //for..of loop iterating through each element in a given array
      if (callback(el)) { //checking if the value from invoking the callback function on each element is truthy
@@ -45,11 +45,11 @@ const myFind = (arr, callback) => {
 // const myNames = ['Alice', 'Bob', 'Charlie', 'Debbie', 'Barry'];
 
 // const nameHasB = myFind(myNames, (name) => name.includes('B'));
-// console.log(nameHasB); // 'Bob' not 'Barry' because 'Bob' is first
+// console.log(nameHasB); //'Bob' not 'Barry' because 'Bob' is first
 // const nameHasZ = myFind(myNames, (name) => name.includes('Z'));
-// console.log(nameHasZ); // undefined
+// console.log(nameHasZ); //undefined
 
-//Question 4: our .Filter version that returns a new array made up of all the values from the array that passed the callback's conditional. it takes an array and a callback, and returns a new array made up of all the values that passed the callback test. if no matches are found, it returns an empty array. do not modify the original array
+//Question 4: a .filter version that returns a new array made up of all the values from the array that passed the callback's conditional. it takes an array and a callback, and returns a new array made up of all the values that passed the callback test. if no matches are found, it returns an empty array. do not modify the original array
 const myFilter = (arr, callback) => {
   let filtArr = []; //initializing a new array to hold our filtered elements in
   for (let el of arr) { //for..of loop iterating through each element in a given array
@@ -63,13 +63,36 @@ const myFilter = (arr, callback) => {
 // //printing q4 test to the console below
 // const myNames = ['Alice', 'Bob', 'Charlie', 'Debbie', 'Barry'];
 // const namesWithB = myFilter(myNames, (name) => name.includes('B'));
-// console.log(namesWithB); // ['Bob', 'Barry']
+// console.log(namesWithB); //['Bob', 'Barry']
 
 // const namesWithZ = myFilter(myNames, (name) => name.includes('Z'));
-// console.log(namesWithZ); // []
+// console.log(namesWithZ); //[]
 
-const sortWords = () => {
-};
+//Question 5: a .sort version takes in an array of words and must sort them in ascending order (remember: ascend => start small get big, descending => start big get small). for the purposes of letters, ascending alphabetical means start with "a.". must return the new sorted array; do not modify the passed in array! be careful, is that .sort's default behavior?
+//using .sort() method (.toSorted() method not available in this node.js version yet)
+const sortWords = (arr) => {
+  let sortedArr = [...arr]; //initializing a shallow copy of a given array using the spread operator to not mutate the original array argument given
+  return sortedArr.sort() //returning the new sorted array with the .sort() method invoked on it
+}
+// //ignore- before i realized we are allowed to use sort method; tried to do it from scratch
+// const sortWords = (arr) => {
+//   let sortArr = [];
+//   for (let el of arr) { //getting used to for..of loops; iterating through each element in an array
+//     sortArr.push(el);
+//     console.log(el.charCodeAt(0), sortArr) //to help me debug
+//       if (el.charCodeAt(0) > el.charCodeAt(1)) {
+//         sortArr.push(el);
+//     };
+//   };
+//   return sortArr;
+// };
+
+// //printing q5 test to the console below
+// const unsortedNames = ['Charlie', 'Debbie', 'Alice', 'Bob'];
+// const sortedNames = sortWords(unsortedNames);
+
+// console.log(sortedNames); //['Alice', 'Bob', 'Charlie', 'Debbie']
+// console.log(unsortedNames); //['Charlie', 'Debbie', 'Alice', 'Bob']
 
 const sortNumbers = () => {
 };
